e2b11c9b4be30e9d5e307975f4e2969d5ba7d57e
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2021-02-15 20:43:53 +0000

    comment corrected

--- docker/start-xvfb.sh
+++ docker/start-xvfb.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-export GEOMETRY="${SCREEN_WIDTH}""x""${SCREEN_HEIGHT}""x""${SCREEN_DE
+export GEOMETRY="${SCREEN_WIDTH}""x""${SCREEN_HEIGHT}""x""${SCREEN_DEPTH}"
+
+rm -f /tmp/.X*lock
+
+# Command reference
+# h
