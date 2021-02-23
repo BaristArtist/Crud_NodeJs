8234f270e9963eb3ea5a89a332712d1c38330b7f
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2021-02-23 11:33:18 +0000

    fix cops

--- docker/start-xvfb.sh
+++ docker/start-xvfb.sh
@@ -5,4 +5,5 @@ export GEOMETRY="${SCREEN_WIDTH}""x""${SCREEN_HEIGHT}""x""${SCREEN_DEPTH}"
 rm -f /tmp/.X*lock
 
 # Command reference
-# http://manpages.ubuntu.com/manpages/focal/man1/xvfb-ru
+# http://manpages.ubuntu.com/manpages/focal/man1/xvfb-run.1.html
+# http://manpages.ubuntu.com/manpages/foca
