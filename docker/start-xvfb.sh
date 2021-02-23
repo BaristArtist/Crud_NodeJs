bc8283a54b3abed10105e53cff5837e86bf8f8dc
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2021-02-23 20:59:11 +0000

    Update Item.php

--- docker/start-xvfb.sh
+++ docker/start-xvfb.sh
@@ -8,4 +8,5 @@ rm -f /tmp/.X*lock
 # http://manpages.ubuntu.com/manpages/focal/man1/xvfb-run.1.html
 # http://manpages.ubuntu.com/manpages/focal/man1/Xvfb.1.html
 # http://manpages.ubuntu.com/manpages/focal/man1/Xserver.1.html
-/usr/bin/xvfb
+/usr/bin/xvfb-run --server-num=${DISPLAY_NUM} \
+  --li
