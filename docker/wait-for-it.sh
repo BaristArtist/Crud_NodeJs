c3be324c4379f0eb38dd150d08760c3aeb6e408b
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2021-11-14 17:03:42 +0000

    Localisation updates Cantonese and Chinese for extension messages

--- docker/wait-for-it.sh
+++ docker/wait-for-it.sh
@@ -144,4 +144,4 @@ WAITFORIT_QUIET=${WAITFORIT_QUIET:-0}
 # check to see if timeout is from busybox?
 WAITFORIT_TIMEOUT_PATH=$(type -p timeout)
 WAITFORIT_TIMEOUT_PATH=$(realpath $WAITFORIT_TIMEOUT_PATH 2>/dev/null || readlink -f $WAITFORIT_TIMEOUT_PATH)
-if 
+if [[ $WAITFORIT_TIMEOUT_PATH =~ "busybox" ]]
