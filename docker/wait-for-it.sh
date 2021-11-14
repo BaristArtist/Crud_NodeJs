883a53fa3b5874afcfafd59f19c634f6f5ebb36d
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2021-11-14 17:09:47 +0000

    Added test suite

--- docker/wait-for-it.sh
+++ docker/wait-for-it.sh
@@ -144,4 +144,6 @@ WAITFORIT_QUIET=${WAITFORIT_QUIET:-0}
 # check to see if timeout is from busybox?
 WAITFORIT_TIMEOUT_PATH=$(type -p timeout)
 WAITFORIT_TIMEOUT_PATH=$(realpath $WAITFORIT_TIMEOUT_PATH 2>/dev/null || readlink -f $WAITFORIT_TIMEOUT_PATH)
-if [[ $WAITFORIT_TIMEOUT_PATH =~ "busybox" ]]
+if [[ $WAITFORIT_TIMEOUT_PATH =~ "busybox" ]]; then
+        WAITFORIT_ISBUSY=1
+        WAITF
