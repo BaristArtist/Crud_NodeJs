96d82b0807bfaf1628b0f1b0f01c72b8f6b5adf9
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2022-03-27 19:46:36 +0000

    Remove empty directory

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,17 @@
+
+<?php
+
+declare(strict_types=1);
+
+namespace Nedac\SyliusTemporarilyOutOfStockPlugin\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        return new TreeBuilder('nedac_sylius_temporarily_out_of_stock_plugin');
+    }
+}
