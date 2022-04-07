d05827299a9fe7a06aa8c5d061ed4ac0c975df1d
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2022-04-07 14:08:37 +0000

    Scheduler

--- src/DependencyInjection/NedacSyliusTemporarilyOutOfStockExtension.php
+++ src/DependencyInjection/NedacSyliusTemporarilyOutOfStockExtension.php
@@ -2,4 +2,6 @@
 
 declare(strict_types=1);
 
-namespace Nedac\SyliusTemporarilyOutOfStockPlugin\Depe
+namespace Nedac\SyliusTemporarilyOutOfStockPlugin\DependencyInjection;
+
+use Symfony\Component\Config
