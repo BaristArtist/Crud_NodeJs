449069eaa4010eb9b68ff43a6dd784a26dfa659b
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2022-04-24 05:37:53 +0000

    update versions for hotfix

--- src/DependencyInjection/NedacSyliusTemporarilyOutOfStockExtension.php
+++ src/DependencyInjection/NedacSyliusTemporarilyOutOfStockExtension.php
@@ -7,4 +7,5 @@ namespace Nedac\SyliusTemporarilyOutOfStockPlugin\DependencyInjection;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Ex
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\Depe
