ed28ae695b628107846630274fd313ae11588f3e
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2022-04-24 08:08:15 +0000

    Input: elantech - fix detection of touchpads where the revision matches a known rate

--- src/DependencyInjection/NedacSyliusTemporarilyOutOfStockExtension.php
+++ src/DependencyInjection/NedacSyliusTemporarilyOutOfStockExtension.php
@@ -9,3 +9,6 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Webmozart\Assert\Assert;
+
+final class N
