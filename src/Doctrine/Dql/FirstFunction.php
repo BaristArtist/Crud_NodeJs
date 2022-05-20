1fc8b8c64de802a8a2f1128fa4d2c53542889cf7
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2022-05-20 18:41:41 +0000

    libceph: eliminate connection state "DEAD"

--- src/Doctrine/Dql/FirstFunction.php
+++ src/Doctrine/Dql/FirstFunction.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 namespace Nedac\SyliusTemporarilyOutOfStockPlugin\Doctrine\Dql;
 
 use Doctrine\ORM\Query\AST\ASTException;
-use Doctrine\ORM\Query\AST\Funct
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\A
