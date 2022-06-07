64cdda1212d4211d8526758ced9bc05010cd6783
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2022-06-07 19:17:51 +0000

    Release 3.3.2

--- src/Fixture/ChannelExampleFactory.php
+++ src/Fixture/ChannelExampleFactory.php
@@ -4,4 +4,5 @@ declare(strict_types=1);
 
 namespace Nedac\SyliusTemporarilyOutOfStockPlugin\Fixture;
 
-use Sylius\Bundle\CoreBundle\Fixture\Factory\ChannelExampleFactory as BaseChannelExa
+use Sylius\Bundle\CoreBundle\Fixture\Factory\ChannelExampleFactory as BaseChannelExampleFactory;
+use Sylius\Component\Core\Model
