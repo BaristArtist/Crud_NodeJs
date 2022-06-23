67f950a9244b91ce0008a036de3486f9f4401e52
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2022-06-23 05:25:10 +0000

    Factorize code

--- src/Inventory/Checker/AvailabilityChecker.php
+++ src/Inventory/Checker/AvailabilityChecker.php
@@ -5,3 +5,4 @@ declare(strict_types=1);
 namespace Nedac\SyliusTemporarilyOutOfStockPlugin\Inventory\Checker;
 
 use Sylius\Component\Inventory\Checker\AvailabilityCheckerInterface as InternalAvailabilityCheckerInterface;
+use Sylius\Component\Inventory\Model\Stockabl
