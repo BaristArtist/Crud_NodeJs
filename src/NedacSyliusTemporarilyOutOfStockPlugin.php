e72ed60202b8e3e7326f779097ba79ce8a1f8143
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2022-07-21 06:18:17 +0000

    Update module.php

--- src/NedacSyliusTemporarilyOutOfStockPlugin.php
+++ src/NedacSyliusTemporarilyOutOfStockPlugin.php
@@ -4,4 +4,5 @@ declare(strict_types=1);
 
 namespace Nedac\SyliusTemporarilyOutOfStockPlugin;
 
-use Sylius\Bundle\CoreBundle\Application\SyliusPlug
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle
