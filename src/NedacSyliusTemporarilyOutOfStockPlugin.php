370f58b18a348a6b6836a7ac2216fa0733e9c5f4
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2022-07-21 12:39:52 +0000

    Updated Travis CI configuration.

--- src/NedacSyliusTemporarilyOutOfStockPlugin.php
+++ src/NedacSyliusTemporarilyOutOfStockPlugin.php
@@ -5,4 +5,6 @@ declare(strict_types=1);
 namespace Nedac\SyliusTemporarilyOutOfStockPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
-use Symfony\Component\HttpKernel\Bundle\Bundle
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class NedacSyliusTemporarilyOu
