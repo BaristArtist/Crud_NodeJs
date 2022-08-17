73ea701336c26f966e3bed3df6dcbd61dd57dd91
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2022-08-17 16:23:24 +0000

    sm-110 Реализация мод workflow-zf2-toolkit

--- src/Repository/ProductRepositoryTrait.php
+++ src/Repository/ProductRepositoryTrait.php
@@ -35,4 +35,5 @@ trait ProductRepositoryTrait
                 ->innerJoin('productTaxon.taxon', 'taxon')
                 ->andWhere('taxon.left >= :taxonLeft')
                 ->andWhere('taxon.right <= :taxonRight')
-                ->andWhere('taxon.root = :taxon
+                ->andWhere('taxon.root = :taxonRoot')
+                ->setParameter('t
