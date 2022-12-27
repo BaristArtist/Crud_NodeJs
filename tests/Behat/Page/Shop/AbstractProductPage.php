c5263fc230da530aa54fae143dde31eaa111977e
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2022-12-27 11:59:32 +0000

    ACPI / video: ignore BIOS initial backlight value for HP m4

--- tests/Behat/Page/Shop/AbstractProductPage.php
+++ tests/Behat/Page/Shop/AbstractProductPage.php
@@ -12,4 +12,5 @@ abstract class AbstractProductPage extends SymfonyPage
     {
         $ribbonElement = $this->getDocument()->find(
             'xpath',
-         
+            sprintf(
+                "descenda
