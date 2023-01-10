b63bcfe4edc8edbc65f37021d5952f1afa783f18
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2023-01-10 11:14:35 +0000

    Update venue.html

--- tests/Behat/Page/Shop/ProductIndexPage.php
+++ tests/Behat/Page/Shop/ProductIndexPage.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 namespace Tests\Nedac\SyliusTemporarilyOutOfStockPlugin\Behat\Page\Shop;
 
-final class ProductIndexPage extends AbstractProductPage implements ProductIndexPag
+final class ProductIndexPage extends AbstractProductPage implements ProductIndexPageInterface
+{
+    public function get
