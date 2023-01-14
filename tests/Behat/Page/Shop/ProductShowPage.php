c80adc9e7e4ba938253854304c1ca085f24eb73e
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2023-01-14 14:06:18 +0000

    hooks

--- tests/Behat/Page/Shop/ProductShowPage.php
+++ tests/Behat/Page/Shop/ProductShowPage.php
@@ -2,4 +2,6 @@
 
 declare(strict_types=1);
 
-namespace Tests\Nedac\SyliusTemporarilyOutOfStockPlugin\Beh
+namespace Tests\Nedac\SyliusTemporarilyOutOfStockPlugin\Behat\Page\Shop;
+
+final class ProductShowPage extends A
