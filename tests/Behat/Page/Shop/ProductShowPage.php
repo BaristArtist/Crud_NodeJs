b746349fda6053f92d9e9e2d5c664a8688ff78af
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2023-01-14 16:50:40 +0000

    Latest Commit from sublime text

--- tests/Behat/Page/Shop/ProductShowPage.php
+++ tests/Behat/Page/Shop/ProductShowPage.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 namespace Tests\Nedac\SyliusTemporarilyOutOfStockPlugin\Behat\Page\Shop;
 
-final class ProductShowPage extends A
+final class ProductShowPage extends AbstractProductPage implements ProductS
