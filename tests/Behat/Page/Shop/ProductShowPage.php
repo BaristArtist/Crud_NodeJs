0eec908d409b4c555afd161cd3aaae8cdd3cabcb
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2023-01-15 06:29:49 +0000

    Updating CHANGELOG.

--- tests/Behat/Page/Shop/ProductShowPage.php
+++ tests/Behat/Page/Shop/ProductShowPage.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 namespace Tests\Nedac\SyliusTemporarilyOutOfStockPlugin\Behat\Page\Shop;
 
-final class ProductShowPage extends AbstractProductPage implements ProductS
+final class ProductShowPage extends AbstractProductPage implements ProductShowPageInterface
+{
+    public function isRibbon
