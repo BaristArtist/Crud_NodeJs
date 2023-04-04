5879af82d5fa33872f7e8416b7efa34ca225776b
BaristArtist <96530210+BaristArtist@users.noreply.github.com>
2023-04-04 15:27:29 +0000

    nfsd: check passed socket's net matches NFSd superblock's one

--- tests/Unit/Inventory/Checker/AvailabilityCheckerTest.php
+++ tests/Unit/Inventory/Checker/AvailabilityCheckerTest.php
@@ -4,3 +4,59 @@
 declare(strict_types=1);
 
 namespace Tests\Nedac\SyliusTemporarilyOutOfStockPlugin\Unit\Inventory\Checker;
+
+use Mockery;
+use Nedac\SyliusTemporarilyOutOfStockPlugin\Inventory\Checker\AvailabilityChecker;
+use PHPUnit\Framework\TestCase;
+use Sylius\Component\Inventory\Checker\AvailabilityCheckerInterface as InternalAvailabilityCheckerInterface;
+use Sylius\Component\Inventory\Model\StockableInterface;
+
+final class AvailabilityCheckerTest extends TestCase
+{
+    private function getInternalAvailabilityCheckerMock(
+        bool $isStockAvailable = false
+    ): InternalAvailabilityCheckerInterface {
+        $checker = Mockery::mock(InternalAvailabilityCheckerInterface::class);
+
+        $checker
+            ->shouldReceive('isStockAvailable')
+            ->andReturn($isStockAvailable)
+        ;
+
+        return $checker;
+    }
+
+    public function testCanInstantiate(): void
+    {
+        self::expectNotToPerformAssertions();
+
+        new AvailabilityChecker($this->getInternalAvailabilityCheckerMock());
+    }
+
+    public function testReturnsFalseWhenNoStockIsAvailable(): void
+    {
+        $mock = Mockery::mock(StockableInterface::class);
+
+        $availabilityChecker = new AvailabilityChecker($this->getInternalAvailabilityCheckerMock(false));
+        $result = $availabilityChecker->isStockAvailable([$mock]);
+
+        self::assertFalse($result);
+    }
+
+    public function testReturnsTrueIfAtLeastOneHasStock(): void
+    {
+        $mock = Mockery::mock(StockableInterface::class);
+
+        $availabilityChecker = new AvailabilityChecker($this->getInternalAvailabilityCheckerMock(true));
+        $result = $availabilityChecker->isStockAvailable([$mock]);
+
+        self::assertTrue($result);
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+
+        Mockery::close();
+    }
+}
